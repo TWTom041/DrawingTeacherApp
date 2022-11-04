1b26c507283894628097e72e5b0aedc636fc228e
TWTom <tom0986820022@gmail.com>
2022-11-04 13:01:11 +0800

    1

--- DrawingTeacher/www/js/drawing_part.js
+++ DrawingTeacher/www/js/drawing_part.js
@@ -4,6 +4,16 @@ let video = document.querySelector("#videoElement")
 let src, dst, cap, height, width;
 let current_step = 0;
 
+document.querySelector("#size-modifier").onchange = function () {
+    let mag = this.value/100;
+    document.querySelector("#processed").getContext("2d").scale(mag, mag);
+}
+document.querySelector("#margintop").onchange = function () {
+    let mar = this.value.toString();
+    document.querySelector(".drawing_window").style.marginTop = mar;
+    document.querySelector(".now_drawing_window").style.marginTop = mar;
+}
+
 if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({audio: false, video: {facingMode: "environment"}})
         .then(function (stream) {
@@ -21,10 +31,10 @@ function back() {
 function nextmove() {
     let steps = JSON.parse(localStorage.getItem("steps"))
     if (current_step < steps.length) {
+        document.querySelector("#processing").getContext("2d").fillStyle = "red";
+        document.querySelector("#processing").getContext("2d").clearRect(0,0,document.querySelector("#processing").width, document.querySelector("#processing").height);
         steps[current_step]["dot_indexes"].forEach(index => {
             document.querySelector("#processed").getContext("2d").fillRect(index[1],index[0],1,1);
-            document.querySelector("#processing").getContext("2d").fillStyle = "red";
-            document.querySelector("#processing").getContext("2d").clearRect(0,0,document.querySelector("#processing").width, document.querySelector("#processing").height);
             document.querySelector("#processing").getContext("2d").fillRect(index[1],index[0],1,1);
         })
     }
