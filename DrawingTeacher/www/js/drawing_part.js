a2ba1102b1ac8aeaa257cbc73758de86269c58f0
TWTom041 <s10855041@thjh.tp.edu.tw>
2022-11-05 01:32:14 +0800

    1

--- DrawingTeacher/www/js/drawing_part.js
+++ DrawingTeacher/www/js/drawing_part.js
@@ -1,17 +1,21 @@
 document.querySelector(".back").addEventListener("click", back)
-document.querySelector(".yes").addEventListener("click", nextmove)
+document.querySelector(".yes").addEventListener("click", nextStep)
 let video = document.querySelector("#videoElement")
-let src, dst, cap, height, width;
+let current_maps = [];
 let current_step = 0;
+let mouseDown = false;
+let all_maps = JSON.parse(localStorage.getItem("steps"))
 
-document.querySelector("#size-modifier").onchange = function () {
-    let mag = this.value/100;
-    document.querySelector("#processed").getContext("2d").scale(mag, mag);
+document.querySelector("#size-modifier").addEventListener("input", changeSize)
+document.querySelector("#margintop").addEventListener("input", addMargin)
+document.body.addEventListener("mousemove", drawUserLine)
+
+document.querySelector("#painting").onmousedown = function() {
+    mouseDown = true;
 }
-document.querySelector("#margintop").onchange = function () {
-    let mar = this.value.toString();
-    document.querySelector(".drawing_window").style.marginTop = mar;
-    document.querySelector(".now_drawing_window").style.marginTop = mar;
+
+document.querySelector("#painting").onmouseup = function() {
+    mouseDown = false;
 }
 
 if (navigator.mediaDevices.getUserMedia) {
@@ -28,15 +32,55 @@ function back() {
     window.location = "home.html"
 }
 
-function nextmove() {
-    let steps = JSON.parse(localStorage.getItem("steps"))
-    if (current_step < steps.length) {
+function changeSize() {
+    let mag = this.value/100;
+    document.querySelector("#processed").width = 384 * mag
+    document.querySelector("#processed").height = 384 * mag
+    document.querySelector("#processing").width = 384 * mag
+    document.querySelector("#processing").height = 384 * mag
+    document.getElementsByClassName("drawing_window")[0].style.width = (384 * mag).toString()+"px"
+    document.getElementsByClassName("drawing_window")[0].style.height = (384 * mag).toString()+"px"
+    document.getElementsByClassName("now_drawing_window")[0].style.width = (384 * mag).toString()+"px"
+    document.getElementsByClassName("now_drawing_window")[0].style.height = (384 * mag).toString()+"px"
+    draw(current_maps)
+}
+
+function addMargin() {
+    let mar = this.value.toString();
+    document.getElementsByClassName("drawing_window")[0].style.marginTop = mar.toString()+"px";
+    document.getElementsByClassName("now_drawing_window")[0].style.marginTop = mar.toString()+"px";
+}
+
+function draw(steps) {
+    let mag = document.querySelector("#size-modifier").value/100;
+    if (current_step) {
         document.querySelector("#processing").getContext("2d").fillStyle = "red";
         document.querySelector("#processing").getContext("2d").clearRect(0,0,document.querySelector("#processing").width, document.querySelector("#processing").height);
-        steps[current_step]["dot_indexes"].forEach(index => {
-            document.querySelector("#processed").getContext("2d").fillRect(index[1],index[0],1,1);
-            document.querySelector("#processing").getContext("2d").fillRect(index[1],index[0],1,1);
+        document.querySelector("#processed").getContext("2d").clearRect(0,0,document.querySelector("#processed").width, document.querySelector("#processed").height);
+        steps.forEach(indexes => {
+            indexes.forEach(index => {
+                document.querySelector("#processed").getContext("2d").fillRect(index[1]*mag,index[0]*mag,1,1);
+            })
+
+        })
+        steps.at(-1).forEach(index => {
+            document.querySelector("#processing").getContext("2d").fillRect(index[1]*mag,index[0]*mag,1,1);
         })
     }
-    current_step += 1;
+}
+
+function nextStep() {
+    if (current_step < all_maps.length) {
+        current_maps.push(all_maps[current_step]["dot_indexes"])
+        current_step += 1;
+        draw(current_maps)
+    }
+}
+
+function drawUserLine(ev) {
+    document.querySelector("#painting").getContext("2d").fillStyle = "blue";
+    let rect = document.querySelector("#painting").getBoundingClientRect();
+    if (mouseDown) {
+        document.querySelector("#painting").getContext("2d").fillRect(ev.clientX - rect.left, ev.clientY - rect.top,3,3);
+    }
 }
